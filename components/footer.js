031a64bc506756e38a19c2adf09cc3673b4bfcb8
sanjeet kumar <skc0845@gmail.com>
2023-04-29 21:07:45 +0530

    file added

---
+++ components/footer.js
@@ -0,0 +1,82 @@
+export const footer = () => {
+  return (
+    `<div>
+
+        <footer class="text-white text-center text-lg-start" style="background-color: #23242a;">
+          <div class="container p-4">
+            <div class="row mt-4">
+              <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase mb-4"><b>About company</b></h5>
+      
+                <p>
+                Spice Food, a culinary journey that takes you on a flavorful adventure. Established in 2020, 
+                we are a restaurant that specializes in bringing the vibrant and unique flavors of the Spice Islands
+                 to your table.
+                </p>
+      
+                <p>
+                At Spice Food, we are committed to providing our guests with an unforgettable dining experience. 
+                </p>
+      
+                <div class="mt-4">
+                  <!-- Facebook -->
+                  <a type="button" class="btn btn-floating btn-warning btn-lg" target="_blank" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                  <!-- Instagram -->
+                  <a type="button" class="btn btn-floating btn-warning btn-lg" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                  <!-- Twitter -->
+                  <a type="button" class="btn btn-floating btn-warning btn-lg" target="_blank" href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                  <!-- Linkedin -->
+                  <a type="button" class="btn btn-floating btn-warning btn-lg" target="_blank" href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a>
+                </div>
+              </div>
+           
+              <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <ul class="fa-ul" style="margin-left: 1.65em;">
+                  <li class="mb-3">
+                    <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">Laxmi Nagar, 110092, Delhi</span>
+                  </li>
+                  <li class="mb-3">
+                    <span class="fa-li"><i class="fas fa-envelope"></i></span><span class="ms-2">contact@spiceifood.com</span>
+                  </li>
+                  <li class="mb-3">
+                    <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">+91 8709682741</span>
+                  </li>
+                  <li class="mb-3">
+                    <span class="fa-li"><i class="fas fa-print"></i></span><span class="ms-2">+91-11-67589089</span>
+                  </li>
+                </ul>
+              </div>
+              
+              <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase mb-4"><b>Opening hours</b></h5>
+      
+                <table class="table text-center text-white">
+                  <tbody class="font-weight-normal">
+                    <tr>
+                      <td>Mon - Thu:</td>
+                      <td>10am - 11pm</td>
+                    </tr>
+                    <tr>
+                      <td>Fri - Sat:</td>
+                      <td>10am - 06pm</td>
+                    </tr>
+                    <tr>
+                      <td>Sunday:</td>
+                      <td>9am - 10pm</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div> 
+          </div>
+          <!-- Copyright -->
+          <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2023 Copyright:
+            <a class="text-white">Spice Food</a>
+          </div>
+          <!-- Copyright -->
+        </footer>
+        
+      </div>`
+  );
+};
